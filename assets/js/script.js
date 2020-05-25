fda171251279f655b9b8d3aa96d3b30951bbd17a
Yadira Tamarit <ystamaritq@gmail.com>
2020-05-25 16:46:03 -0500

    adjusting website

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,7 +55,7 @@ function loadWeather(city) {
 			var conditionIcon = data.day.condition.icon;
 
 			$("#days-wrap").append(`
-				<div class="card col-lg-2 col-sm-12 py-2 mr-1 bg-primary text-white mt-2">
+				<div class="card col-xl-2 col-lg-4 col-sm-3 py-2 mr-1 bg-primary text-white mt-2">
 					<span class="card-title pb-3">${dateForecast}</span>
 		   		    <img src="https:${conditionIcon}" width=64>
 		  		    <span>Temp: ${tempForecast} F</span>
